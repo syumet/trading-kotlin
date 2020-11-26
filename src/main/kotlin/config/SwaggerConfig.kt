7c070be5e52cdb00df44a4df1509a0f38da2fd43
wxlxy95 <liuxinyu9090@gmail.com>
2020-11-26 01:34:49 -0500

    config profiles, add swagger-ui

---
+++ src/main/kotlin/config/SwaggerConfig.kt
@@ -0,0 +1,56 @@
+package ca.jrvs.trading.config
+
+import com.google.common.base.Predicates
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger.web.*
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+    @Bean
+    fun docket(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/")
+                .apiInfo(ApiInfo(
+                        "Trading App",
+                        "POC trading backend with Kotlin and Spring Boot",
+                        null,
+                        null,
+                        null,
+                        "MIT",
+                        null,
+                        listOf())
+                )
+    }
+
+    @Bean
+    fun uiConfig(): UiConfiguration {
+        return UiConfigurationBuilder.builder()
+                .deepLinking(true)
+                .displayOperationId(false)
+                .defaultModelsExpandDepth(1)
+                .defaultModelExpandDepth(1)
+                .defaultModelRendering(ModelRendering.MODEL)
+                .displayRequestDuration(true)
+                .docExpansion(DocExpansion.NONE)
+                .filter(false)
+                .maxDisplayedTags(null)
+                .operationsSorter(OperationsSorter.METHOD)
+                .showExtensions(false)
+                .tagsSorter(TagsSorter.ALPHA)
+                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+                .validatorUrl(null)
+                .build()
+    }
+}
